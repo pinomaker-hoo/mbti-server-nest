e1b71b35f37a3c9629b060a9d6cf97153e8decff
pino._.hoo <inhoo23@naver.com>
2022-11-15 02:00:03 +0900

    add : Auth Module, Jwt, config

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthService } from './application/auth.service';
 import { UserRepository } from './infrastructure/user.repository';
+import { JwtStrategy } from './passport/auth.jwt.strategy';
+import { KakaoStrategy } from './passport/auth.kakao.strategy';
 import AuthController from './ui/auth.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
-  providers: [AuthService],
+  imports: [
+    ConfigModule,
+    TypeOrmModule.forFeature([UserRepository]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get('JWT_EXPIRESIN'),
+        },
+      }),
+    }),
+  ],
+  providers: [AuthService, JwtStrategy, KakaoStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
