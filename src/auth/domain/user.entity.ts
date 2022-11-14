33adb2a80f531ed078c574eb7243a307b93df81e
pino._.hoo <inhoo23@naver.com>
2022-11-15 01:46:35 +0900

    add : Auth Module

---
+++ src/auth/domain/user.entity.ts
@@ -0,0 +1,17 @@
+import { BaseTimeEntity } from 'src/common/entity/BaseTime.Entity';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'tbl_user' })
+export class User extends BaseTimeEntity {
+  @PrimaryGeneratedColumn()
+  idx: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  providerIdx: string;
+}
